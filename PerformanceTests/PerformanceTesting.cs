12997762911d3bb3e0bfbbb83bc9a98d0e647b49
CMukh <41068300+CMukh@users.noreply.github.com>
2018-08-27 16:37:42 +0530

    Added Performance Test

---
+++ PerformanceTests/PerformanceTesting.cs
@@ -0,0 +1,39 @@
+﻿using NBench;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTests
+{
+   
+        class PerformanceTesting : PerformanceTestStuite<PerformanceTesting>
+        {
+            private readonly Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            private const string AddCounterName = "AddCounter";
+            private Counter addCounter;
+            private int key;
+            private const int AcceptableMinAddThroughput = 200;
+
+            [PerfSetup]
+            public void Setup(BenchmarkContext context)
+            {
+                addCounter = context.GetCounter(AddCounterName);
+                key = 0;
+            }
+
+            [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Test)]
+            [CounterThroughputAssertion(AddCounterName, MustBe.GreaterThan, AcceptableMinAddThroughput)]
+            public void AddThroughput_IterationsMode(BenchmarkContext context)
+            {
+                for (var i = 0; i < AcceptableMinAddThroughput; i++)
+                {
+                    dictionary.Add(i, i);
+                    addCounter.Increment();
+                }
+            }
+        }
+    
+}
